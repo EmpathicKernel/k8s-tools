d1ebc972142c9dc83ecd6d6765b442a27a1e9ae0
Jordon Kersey <jordon.kersey@greenlight.guru>
2024-04-11 10:10:38 -0400

    Documentation and --namespace-wide option

--- deployment-scaling/main.py
+++ deployment-scaling/main.py
@@ -1,3 +1,39 @@
+###############################
+# This script is used to easily scale deployments up or down 
+#
+# Options:
+#   -n, --namespace             Specify namespace to run against (Required)
+#   --loglevel                  Log level to output. Defaults to "info". Options are: debug, info, warning, error
+#   --timeout                   Timeout in seconds for the API calls. Defaults to 60
+#   --release-label             Deployment label that maps to the Helm release name. Defaults to "release"
+#   --scale-in                  Flag to determine if deployments should be scaled in. Will scale deployments to 
+#                                  replica count from Helm release or replica count passed through --replicas option. If
+#                                  no Helm release is found and no replica count is passed in, it will default to 1. Will not
+#                                  scale higher than number of current replicas
+#   --scale                     Flag to determine if deployments should be scaled out. Will scale deployments to 
+#                                   replica count from Helm release or replica count passed through --replicas option. If
+#                                  no Helm release is found and no replica count is passed in, it will default to 1. Will not
+#                                  scale lower than number of current replicas
+#   --replicas                  Number of replicas to scale to. Defaults to 1
+#   --no-helm-replica-check     Flag to skip the check of Helm to find replica count. Requires --replicas to be defined
+#   --namespace-wide            Flag to determine if full namespace should be scaled. If not set, deployments must be passed in
+#                                   unless no scaling is occurring
+#   
+# Arguments:
+#   Specific deployment names can be passed as additional arguments. Will override --namespace-wide if used in conjunction
+# 
+# Usage:
+#   
+#       Scale out the number of replicas for entire namespace "default" to 4
+#           python3 ./k8s_scaling.py -n default --scale --namespace-wide --replicas 4
+#
+#       Scale in the number of replicas for example-deployment within namespace "default" to 0
+#           python3 ./k8s_scaling.py -n default --scale-in --replicas 0 example-deployment
+#
+#       Get a printout of current number of replicas within namespace "test-namespace"
+#           python3 ./k8s_scaling.py -n test-namespace
+# 
+
 import sys
 import logging
 import optparse
@@ -11,11 +47,12 @@ parser = optparse.OptionParser()
 parser.add_option("-n", "--namespace", dest = "namespace", action = "store", help = "Specify namespace to run against")
 parser.add_option("--loglevel", dest = "loglevel", action = "store", default = "INFO", help = "Log level to output")
 parser.add_option("--timeout", dest = "timeout", action = "store", default = 60, help = "Timeout for Kubernete API calls")
-parser.add_option("--release-name", dest = "releaseName", action = "store", default = "release", help = "Label that maps to helm release name")
+parser.add_option("--release-label", dest = "releaseLabel", action = "store", default = "release", help = "Label that maps to helm release name")
 parser.add_option("--scale-in", dest = "scaleIn", action = "store_true", help = "Flag to determine if deployments should be scaled in. Cannot be used with --scale")
 parser.add_option("--scale", dest = "scaleOut", action = "store_true", help = "Flag to determine if deployments should be scaled out. Cannot be used with --scale-in")
 parser.add_option("--replicas", dest = "replicas", action = "store", help = "Number of replicas to scale to. Does not overwrite Helm replica count. Must be used with either --scale or --scale-in")
 parser.add_option("--no-helm-replica-check", dest = "helmCheck", action = "store_false", default = True, help = "Skip checking Helm charts for replica counts. If used, --replicas must be specified.")
+parser.add_option("--namespace-wide", dest = "namespaceWide", action = "store_true", default = False, help = "Scale all deployments within the namespace. Overridden by deployments passed in as arguments")
 
 options, args = parser.parse_args()
 
@@ -23,22 +60,27 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',level=options
 
 selDeployments = []
 
+def optionError(message):
+    logging.error(f"{message}... Exiting")
+    sys.exit()
+
 if options.namespace:
     Namespace = options.namespace
 else:
-    logging.error("Namespace not supplied... Exiting...")
-    sys.exit()
+    optionError("Namespace not supplied")
 
 if options.scaleIn and options.scaleOut:
-    logging.error("You cannot specify both a scale-in and scale-out event")
-    sys.exit()
+    optionError("You cannot specify both a scale-in and scale-out event")
 else:
     ScaleIn = options.scaleIn
     ScaleOut = options.scaleOut
 
 if not options.helmCheck and not options.replicas:
-    logging.error("Replicas must be specified when not running a Helm check")
-    sys.exit()
+    optionError("Replicas must be specified when not running a Helm check")
+
+print(not (ScaleIn or ScaleOut))
+if not options.namespaceWide and not args and (ScaleIn or ScaleOut):
+    optionError("Deployments must be passed in as arguments unless --namespace-wide is used or no scaling is occurring")
 
 DefaultReplicas = 1
 UserDefaultedReplicas = False
@@ -50,9 +92,12 @@ if args:
     logging.info(f"Only looking for these deployments: {args}")
     selDeployments = args
 
+
 Timeout = options.timeout
-GlobalReleaseName = options.releaseName
+GlobalReleaseLabel = options.releaseLabel
 HelmCheck = options.helmCheck
+
+
 
 
 def listDeploymentInfo(deployList):
@@ -83,8 +128,8 @@ def findReleaseNames(deployments):
 
     for item in deployments:
         deployment_name = item.metadata.name
-        release = item.metadata.labels.get(GlobalReleaseName, None)
-        logging.debug(f"Deployment {deployment_name} has label {GlobalReleaseName} of {release}")
+        release = item.metadata.labels.get(GlobalReleaseLabel, None)
+        logging.debug(f"Deployment {deployment_name} has label {GlobalReleaseLabel} of {release}")
         deployedReleases[deployment_name] = {"helmRelease" : release}
 
     return deployedReleases
@@ -208,15 +253,17 @@ def main():
 
     deployments = buildDeploymentData()
 
-    logging.debug(f"Deployment data:\n{deployments}")
+    logging.debug(f"Replica data:\n{deployments}")
 
     if ScaleIn or ScaleOut:
-        answer = input(f"Deployment Data:\n{deployments}\n\nContinue with scaling? [(Y)es/(N)o]:  ")
+        answer = input(f"Replica Data:\n{deployments}\n\nContinue with scaling? [(Y)es/(N)o]:  ")
         if answer.lower() in ["y","yes"]:
             for deployment in deployments:
                 scaleDeployment(deployment, deployments[deployment])
         else:
             logging.info(f"Scaling aborted...")
+    else:
+        logging.info(f"Current replica data:\n{deployments}")
 
 
 if __name__ == "__main__":
